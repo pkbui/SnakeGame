af3bfa3dfc9412d4bc121322a29f50cdb1cfe253
Thai Quoc Nguyen <thaiquocnguyen@outlook.com>
2017-04-23 21:08:30 -0700

    Main window

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -1,8 +1,26 @@
 #include <SFML/Window.hpp>
-
+#include <SFML/Graphics.hpp>
 int main()
 {
-	sf::Window window(sf::VideoMode(800, 600), "MainWindow");
+	sf::RenderWindow window(sf::VideoMode(800, 600), "MainWindow");
+	
+	while (window.isOpen())
+	{
+		// Event handling
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+			{
+				window.close();
+			}
+		}
+
+		// Game stuff here
+
+		// draw to screen
+		window.display();
+	}
 
 	return 0;
 }
