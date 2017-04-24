84cac54a76dc1689db3e24495ea7c98eabd9b3c7
Khang Bui <khang.bui@outlook.com>
2017-04-23 21:09:19 -0700

    commit some code

--- SnakeGame/main.cpp
+++ SnakeGame/main.cpp
@@ -1,8 +1,13 @@
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	sf::Window window(sf::VideoMode(800, 600), "MainWindow");
-
+	sf::RenderWindow window(sf::VideoMode(800, 600), "MainWindow");
+	sf::RectangleShape shape(sf::Vector2f(300, 100));
+	shape.setSize(100, 100);
+	window.draw(shape);
+	window.display();
+	while (true);
 	return 0;
 }
